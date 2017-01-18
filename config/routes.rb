4f1dc53200e53403ada3baf8557786c643c07f9d
Min Khati <meetup.min@gmail.com>
2017-01-18 12:58:04 -0500

    Added UI controller and index.html page and update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
   end
+
+  get '/ui'   => 'ui#index'
+  get '/ui#'  => 'ui#index'
+  root 'ui#index'
 end
